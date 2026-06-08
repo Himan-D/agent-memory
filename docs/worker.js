371628fe90186ff3bdbcfcfdb7333af50d452958
Himan <himanshu.dixit@nexod.ai>
2026-06-08 19:30:53 +0530

    Fix docs subdomain asset routing (#121)

---
+++ docs/worker.js
@@ -0,0 +1,26 @@
+const DOCS_PREFIX = '/docs'
+
+function stripDocsPrefix(pathname) {
+  if (pathname === DOCS_PREFIX) {
+    return '/'
+  }
+  if (pathname.startsWith(DOCS_PREFIX + '/')) {
+    return pathname.slice(DOCS_PREFIX.length) || '/'
+  }
+  return pathname
+}
+
+async function fetchAsset(env, request, pathname) {
+  const url = new URL(request.url)
+  url.pathname = pathname
+  return env.ASSETS.fetch(new Request(url.toString(), request))
+}
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url)
+    const pathname = stripDocsPrefix(url.pathname)
+
+    return fetchAsset(env, request, pathname)
+  },
+}
